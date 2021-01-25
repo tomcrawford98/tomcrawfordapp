dd34bbd283105956885edf654b78616f386d12c1
tomcrawford98 <tomcrawford98@gmail.com>
2021-01-25 01:44:53 +0000

    second commit journal blog 2

--- app/src/main/java/com/example/tomcrawfordapp/RecipeLog.java
+++ app/src/main/java/com/example/tomcrawfordapp/RecipeLog.java
@@ -8,57 +8,34 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link RecipeLog#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
 public class RecipeLog extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public RecipeLog() {
+        FloatingActionButton fab = getActivity().findViewById(R.id.fab);
+        fab.setImageResource (R.drawable.ic_add_location_black_24dp);
+        fab.setOnClickListener (new View.OnClickListener () {
+ @Override
+ public void onClick (View view) {
+                 Snackbar.make(view, " Home␣Fragment␣Action␣Button ", Snackbar.LENGTH_LONG ).setAction(" Action ", null ).show();
+                 }
+ });
+
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RecipeLog.
-     */
     // TODO: Rename and change types and number of parameters
-    public static RecipeLog newInstance(String param1, String param2) {
-        RecipeLog fragment = new RecipeLog();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_recipe_log, container, false);
+
     }
 }
